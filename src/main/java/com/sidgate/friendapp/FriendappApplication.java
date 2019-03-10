e2efbf0bb6ed018beadd0e80d0bae22c2748cc66
Siddharth <sidgate@gmail.com>
2019-03-10 08:45:05 +0530

    spring initializer project

---
+++ src/main/java/com/sidgate/friendapp/FriendappApplication.java
@@ -0,0 +1,13 @@
+package com.sidgate.friendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidgate/friendapp/FriendappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sidgate.friendapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
